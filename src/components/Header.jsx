3c1f8e1180b9229e0be78113f3f304ac43b93b09
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2024-12-21 23:32:55 +0530

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,44 +25,7 @@ const Header = () => {
               </a>
             </li>
             <li>
-              <a href="#" className="nav-link px-2 text-white">
-                Features
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 text-white">
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 text-white">
-                FAQs
-              </a>
-            </li>
-            <li>
-              <a href="#" className="nav-link px-2 text-white">
-                About
-              </a>
-            </li>
           </ul>
-
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-            <input
-              type="search"
-              className="form-control form-control-dark text-bg-dark"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-
-          <div className="text-end">
-            <button type="button" className="btn btn-outline-light me-2">
-              Login
-            </button>
-            <button type="button" className="btn btn-warning">
-              Sign-up
-            </button>
-          </div>
         </div>
       </div>
     </header>
